bbe73e2b43e051997ed89fec6de5262df5fcc956
Olamide Alli <allilmd4@gmail.com>
2022-09-08 14:52:46 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Description: with proper grammar, but the outcome is a piece of art,
  * return : Alway 0
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
