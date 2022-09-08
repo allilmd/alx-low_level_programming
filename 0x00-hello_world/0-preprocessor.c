6cdf6da4e4abd31edea8fec70a13f65447bd787f
Olamide Alli <allilmd4@gmail.com>
2022-09-08 13:13:22 -0700

    0-preprocessor

--- 0x00-hello_world/0-preprocessor.c
+++ 0x00-hello_world/0-preprocessor.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 /* This is a comment */
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
 int main(void)
-{
-	    return (0);
+{	
+	return (0);
 }
