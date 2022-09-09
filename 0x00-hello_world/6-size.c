7b41194a797f4a7a286b43ed36d575f4020571a7
Olamide Alli <allilmd4@gmail.com>
2022-09-09 01:37:40 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - sizes
+ *
+ * return - zer always
+ */
+int main(void)
+{
+	int i;
+	double d;
+	char c;
+
+	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of an double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of an c is: %lu.\n", (unsigned long)sizeof(c));
+
+	return(0);
+}
