36246dc47b89cf7c8a32861ac7383424cdc98dda
Olamide Alli <allilmd4@gmail.com>
2022-09-09 18:46:15 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print_alphabet
+ *
+ * Return: Always 0 (success)
+ */
+
+int main()
+{
+	putchar(101);
+	putchar(10);
+	return (0);
+}
