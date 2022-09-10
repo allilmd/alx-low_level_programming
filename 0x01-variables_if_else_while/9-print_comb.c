fe5787a5ff70eb701deb471fdb0c53ae84671fb8
Olamide Alli <allilmd4@gmail.com>
2022-09-10 03:57:50 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print comb
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+	
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
