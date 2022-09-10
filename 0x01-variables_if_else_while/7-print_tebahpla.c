31bdffcc49d66a179ae9c3c81c40ed52e9183b70
Olamide Alli <allilmd4@gmail.com>
2022-09-10 03:41:50 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : alphabets in reverse
+ *
+ * Return: ALways 0 (success)
+ */
+
+int main(void)
+{
+	char new;
+	
+	for (new = 'z'; new >= 'a'; new--)
+	{
+		putchar(new);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
