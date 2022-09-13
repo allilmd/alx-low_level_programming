6f21d2fceb82eeb1455e8e078348ca42be6d1670
Olamide Alli <allilmd4@gmail.com>
2022-09-12 21:59:27 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 101-print_comb4.c
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a <= 55; a++)
+	{
+		for (b = 49; b <= 56; b++)
+		{
+			for (c = 50; c<= 57; c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					{
+					if (a != 55 || b != 56 || c != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					
+						}	
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
