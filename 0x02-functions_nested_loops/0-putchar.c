7a75eaa8cb408bd6fc3f195bbc2e552f218652e3
Olamide Alli <allilmd4@gmail.com>
2022-09-14 10:54:53 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * mai - entry point
+ *
+ * Description: prints _putchar
+ *
+ * Return: ALways 0 (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
