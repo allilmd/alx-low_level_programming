8d74e85998e5e0ed5dea7e4b7b39c06f40c3f8c8
Olamide Alli <allilmd4@gmail.com>
2022-09-14 19:18:21 -0700

    tabs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs : computes the absolute integer
+ *
+ * @i: the number to check
+ *
+ * Return: The absolute value of i
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	return (-i);
+}
