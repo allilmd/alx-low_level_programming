10072b8a8a6f16503dbb696f86d7a75c31701aa1
Olamide Alli <allilmd4@gmail.com>
2022-09-14 17:13:35 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _sign - prints the sihn of a number
+ *
+ * @n - The integer to be checked
+ *
+ * Return: 1 if n is > 0. 0 if n is= 0. -1 if n is < 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+	}
+	return (-1);
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-alpha
+5-sign
