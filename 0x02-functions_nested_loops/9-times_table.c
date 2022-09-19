d786a8188453377e8a2f9e25128d356abb4a8e99
Olamide Alli <allilmd4@gmail.com>
2022-09-19 03:32:03 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,16 @@ void times_table(void)
 {
 	int a, b;
 
-	a = 9;
+	a = 0;
 
 	while (a < 10)
 	{
 		b = 0;
 		while (b <= 12)
 		{
-			_putchar(b * a);
+			_putchar((b * a) + '0');
 			_putchar(',');
-			_putchar('.');
+			_putchar('\n');
 			b++;
 		}
 		a++;
